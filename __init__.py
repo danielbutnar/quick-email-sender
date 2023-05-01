683904d57e27d4bb73ad2a2077b99e430af9a3e7
Butnar Daniel <128840298+danielbutnar@users.noreply.github.com>
2023-05-02 00:34:47 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .text_processing import TextProcessing
-from .email_handler import EmailHandler
-from .utils import utility_function_1
-from .common_imports import *
-
-__all__ = ['TextProcessing', 'EmailHandler', 'utility_function_1', 'commun_imports']
