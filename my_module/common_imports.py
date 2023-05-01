ea70716ed4dc2fbdb05785b257f032405b5d4957
Butnar Daniel <128840298+danielbutnar@users.noreply.github.com>
2023-05-02 01:18:56 +0300

    Update common_imports.py

--- my_module/common_imports.py
+++ my_module/common_imports.py
@@ -1,3 +1,28 @@
+"""
+This module imports a variety of libraries and tools for text processing, machine learning, email handling, and more. It provides functionality for argument parsing, file and directory management, regular expressions, natural language processing, and other common tasks.
+
+Imports:
+
+argparse: A library for parsing command-line arguments and options.
+os: A library for interacting with the operating system, such as file and directory management.
+re: A library for working with regular expressions.
+smtplib: A library for sending emails using the Simple Mail Transfer Protocol (SMTP).
+concurrent.futures.ThreadPoolExecutor: A class for creating and managing a pool of worker threads for concurrent execution of tasks.
+email.mime: Classes for creating and handling email messages with different MIME types.
+pathlib.Path: A class for working with filesystem paths in a platform-independent manner.
+stanza: A library for natural language processing, providing tokenization, part-of-speech tagging, and more.
+spacy: A library for advanced natural language processing, including tokenization, parsing, and named entity recognition.
+transformers: A library for working with state-of-the-art natural language processing models, such as BERT, GPT, and others.
+openai: A library for working with the OpenAI API.
+nltk: A library for natural language processing, providing tokenization, stemming, and more.
+langid: A library for language identification.
+conceptnet_lite: A library for working with the ConceptNet knowledge graph.
+dotenv: A library for loading environment variables from a .env file.
+pickle: A library for serializing and deserializing Python objects.
+logging: A library for logging messages in a flexible and configurable way.
+To use these libraries in your code, simply import the required modules and functions as needed.
+"""
+
 import argparse
 import os
 import re
@@ -19,3 +44,4 @@ import conceptnet_lite
 from dotenv import load_dotenv
 import pickle
 import logging
+
