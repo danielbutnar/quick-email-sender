43e5f640494e6f04a86c82bab3965c8fa71edd68
Butnar Daniel <128840298+danielbutnar@users.noreply.github.com>
2023-05-02 01:17:51 +0300

    Update __init__.py

--- my_module/__init__.py
+++ my_module/__init__.py
@@ -1,3 +1,18 @@
+"""
+This module provides a collection of utility classes and functions for text processing, email handling, and common imports.
+
+It includes the following classes and functions:
+
+Classes:
+
+TextProcessing: A class containing various methods for processing and manipulating text.
+EmailHandler: A class that provides functionality for handling and processing email-related tasks.
+Functions:
+
+utility_function_1: A general utility function for performing a specific task (e.g., data transformation, parsing, etc.)
+common_imports: A collection of common and frequently used imports to be used across the application.
+"""
+
 from .text_processing import TextProcessing
 from .email_handler import EmailHandler
 from .utils import utility_function_1
